74187b3b06a1acb73c0cc4d8c72019d203db156f
Max <maxmakesgames21@gmail.com>
2023-10-14 07:31:29 -0400

    Enemy waves

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+[Serializable]
+public struct EnemySpawnInfo
+{
+    public Sprite sprite;
+    public Color color;
+}
 
 public class GameManager : MonoBehaviour
 {
@@ -16,6 +24,24 @@ public class GameManager : MonoBehaviour
     }
 
     public int agathaHealth = 10;
+    public EnemySpawnInfo[] enemies; //store enemy templates in here, their array index is their id to use in things like levelEnemyIds
+    public int[] levelEnemyIds;
+    public GameObject enemyPrefab;
+
+    private void Start()
+    {
+        //could spawn them as the level unfolds if needed but for now just spawn them off-screen :sip:
+        Vector3 position = new Vector3(10.0f, 1.03f, 0.0f); //spawn pos for the first enemy
+        for(int i = 0; i < levelEnemyIds.Length; i++)
+        {
+            GameObject enemy = Instantiate(enemyPrefab, position, Quaternion.identity);
+            SpriteRenderer renderer = enemy.GetComponent<SpriteRenderer>();
+            renderer.sprite = enemies[levelEnemyIds[i]].sprite;
+            renderer.color = enemies[levelEnemyIds[i]].color;
+
+            position.x += 2.0f; //distance between enemies
+        }
+    }
 
     public void DamageAgatha(int dmg)
     {
